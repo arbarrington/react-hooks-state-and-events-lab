a8a6371ae50f72525854c92ba4374eadda7acde4
Austin Barrington <arbarrington12@gmail.com>
2022-08-31 11:48:30 -0600

    commit tree 332f82d11be443bae215ef82f6f89d3881c75e77

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [inCart, setInCart] = useState(false);
+
+  function handleAddItem() {
+    console.log("adding");
+    setInCart(!inCart);
+
+  }
+
+  const itemClass = inCart ? "in-cart" : "";
+  const cartOptions = inCart ? "Remove From Cart" : "Add To Cart";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleAddItem}>{cartOptions}</button>
     </li>
   );
 }
